8ea62b3ab6a37f2654f3a23ed6c181c315dfebde
sisber <sisber@twilio.com>
2019-07-30 10:38:24 -0700

    test: testing out on anchor (uncommit this)

--- packages/paste-core/components/anchor/src/styles.ts
+++ packages/paste-core/components/anchor/src/styles.ts
@@ -1,26 +1,30 @@
 import styled from '@emotion/styled';
 import themeGet from '@styled-system/theme-get';
+import css from '@styled-system/css';
 
-export const StyledLink = styled.a(props => ({
-  color: themeGet('textColors.colorTextLink')(props),
-  textDecoration: 'none',
-  outline: 'none',
+export const StyledLink = styled.a(props => {
+  console.log(props);
+  return css({
+    color: 'colorTextLink',
+    textDecoration: 'none',
+    outline: 'none',
 
-  '&:hover': {
-    color: themeGet('textColors.colorTextLinkHover')(props),
-    textDecoration: 'underline',
-  },
+    '&:hover': {
+      color: 'colorTextLinkHover',
+      textDecoration: 'underline',
+    },
 
-  '&:focus, &:active': {
-    boxShadow: '0 0 0 4px rgba(0, 117, 195, 0.5)',
-    textDecoration: 'underline',
-  },
+    '&:focus, &:active': {
+      boxShadow: '0 0 0 4px rgba(0, 117, 195, 0.5)',
+      textDecoration: 'underline',
+    },
 
-  '&:focus': {
-    color: themeGet('textColors.colorTextLinkFocus')(props),
-  },
+    '&:focus': {
+      color: themeGet('textColors.colorTextLinkFocus')(props),
+    },
 
-  '&:active': {
-    color: themeGet('textColors.colorTextLinkActive')(props),
-  },
-}));
+    '&:active': {
+      color: themeGet('textColors.colorTextLinkActive')(props),
+    },
+  });
+});
