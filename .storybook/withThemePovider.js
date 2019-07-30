a28d8f057f3859369425e017bf426aba7d427357
sisber <sisber@twilio.com>
2019-07-30 10:37:53 -0700

    fix: pass the theme into storybook

--- .storybook/withThemePovider.js
+++ .storybook/withThemePovider.js
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import {Theme} from '@twilio-paste/theme';
+import {DefaultTheme} from '@twilio-paste/theme-tokens';
 
-const withThemeProvider = storyFn => <Theme.Provider customBreakpoints={[]}>{storyFn()}</Theme.Provider>;
+const withThemeProvider = storyFn => (
+  <Theme.Provider theme={DefaultTheme} customBreakpoints={[]}>
+    {storyFn()}
+  </Theme.Provider>
+);
 
 export {withThemeProvider};
